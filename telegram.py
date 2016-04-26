139aac3f743ffa687c4c711ea18e2fa4c8d4eace
Andrey 'virus' Yantsen <andrey@janzen.su>
2016-04-26 07:54:58 +0300

    [api] add send_photo; add reply_markup classes

--- telegram.py
+++ telegram.py
@@ -1,11 +1,14 @@
 import logging
+from time import time
 
+from os.path import basename
 from tornado.concurrent import Future
 from tornado.gen import coroutine, Return, sleep
 from tornado.httpclient import AsyncHTTPClient, HTTPError
 import ujson
 
 from tornado.queues import Queue
+from hashlib import md5
 
 
 class Api:
@@ -80,13 +83,59 @@ class Api:
 
     @coroutine
     def __request_api(self, method, body=None, request_timeout=10):
+        def guess_filename(obj):
+            """Tries to guess the filename of the given object."""
+            name = getattr(obj, 'name', None)
+            if name and isinstance(name, str) and name[0] != '<' and name[-1] != '>':
+                return basename(name)
+
         url = 'https://api.telegram.org/bot{token}/{method}'.format(token=self.token, method=method)
         try:
-            response = yield AsyncHTTPClient().fetch(url,
-                                                     method='POST' if body is not None else 'GET',
-                                                     headers={'Content-type': 'application/json'} if body is not None else None,
-                                                     body=ujson.dumps(body) if body is not None else None,
-                                                     request_timeout=request_timeout)
+            request = {
+                'request_timeout': request_timeout,
+                'headers': {},
+            }
+
+            if body:
+                request['method'] = 'POST'
+                request_content = {}
+                has_files = False
+                file_names = {}
+                for key, value in body.items():
+                    if hasattr(value, 'read'):
+                        request_content[key] = value.read()
+                        file_names[key] = guess_filename(value)
+                        has_files = True
+                    else:
+                        request_content[key] = value
+
+                if has_files:
+                    boundary = md5(str(time()).encode('utf-8')).hexdigest()
+                    request['headers']['Content-type'] = 'multipart/form-data; boundary=' + boundary
+
+                    body = []
+                    for key, value in request_content.items():
+                        body.append(b'--' + boundary.encode('utf-8'))
+                        if key in file_names:
+                            body.append(('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, file_names[key])).encode('utf-8'))
+                        else:
+                            body.append(('Content-Disposition: form-data; name="%s"' % key).encode('utf-8'))
+
+                        body.append(b'')
+                        if isinstance(value, int):
+                            value = str(value)
+                        if isinstance(value, str):
+                            value = value.encode('utf-8')
+                        body.append(value)
+                    body.append(b'--' + boundary.encode('utf-8') + b'--')
+                    body = b"\r\n" + b"\r\n".join(body) + b"\r\n"
+                else:
+                    request['headers']['Content-type'] = 'application/json'
+                    body = ujson.dumps(request_content)
+            else:
+                request['method'] = 'GET'
+
+            response = yield AsyncHTTPClient().fetch(url, body=body, **request)
 
             if response and response.body:
                 response = ujson.loads(response.body.decode('utf-8'))
@@ -200,6 +249,26 @@ class Api:
         return (yield self.__request_api('sendMessage', request))
 
     @coroutine
+    def send_photo(self, chat_id, photo, caption: str=None, disable_notification: bool=False,
+                   reply_to_message_id: int=None, reply_markup=None):
+        request = {
+            'chat_id': chat_id,
+            'photo': photo,
+            'disable_notification': disable_notification,
+        }
+
+        if caption is not None:
+            request['caption'] = caption
+
+        if reply_to_message_id is not None:
+            request['reply_to_message_id'] = reply_to_message_id
+
+        if reply_markup is not None:
+            request['reply_markup'] = reply_markup
+
+        return (yield self.__request_api('sendPhoto', request))
+
+    @coroutine
     def forward_message(self, chat_id, from_chat_id, message_id: int, disable_notification: bool=False):
         return (yield self.__request_api('forwardMessage', {
             'chat_id': chat_id,
@@ -293,6 +362,55 @@ class Api:
                 logging.exception('Error while processing message')
 
             self.processing_queue.task_done()
+
+
+class ReplyMarkup(dict):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class InlineKeyboardButton(dict):
+    def __init__(self, text, url=None, callback_data=None, switch_inline_query=None):
+        params = iter((url, callback_data, switch_inline_query))
+        assert any(params) and not any(params)
+        p = {}
+        if url:
+            p['url'] = url
+        elif callback_data:
+            p['callback_data'] = callback_data
+        elif switch_inline_query:
+            p['switch_inline_query'] = switch_inline_query
+        super().__init__(text=text, **p)
+
+
+class InlineKeyboardMarkup(ReplyMarkup):
+    def __init__(self, inline_keyboard: list):
+        assert all(map(lambda x: isinstance(x, list), inline_keyboard))
+        assert all(map(lambda x: all(map(lambda z: isinstance(z, InlineKeyboardButton), x)), inline_keyboard))
+        super().__init__(inline_keyboard=inline_keyboard)
+
+
+class KeyboardButton(dict):
+    def __init__(self, text, request_contact=False, request_location=False):
+        super().__init__(text=text, request_contact=request_contact, request_location=request_location)
+
+
+class ReplyKeyboardMarkup(ReplyMarkup):
+    def __init__(self, keyboard: list, resize_keyboard=False, one_time_keyboard=False, selective=False):
+        assert all(map(lambda x: isinstance(x, list), keyboard))
+        assert all(map(lambda x: all(map(lambda z: isinstance(z, KeyboardButton), x)), keyboard))
+        super().__init__(keyboard=keyboard, resize_keyboard=resize_keyboard, one_time_keyboard=one_time_keyboard,
+                         selective=selective)
+
+
+class ReplyKeyboardHide(ReplyMarkup):
+    def __init__(self, selective=False):
+        super().__init__(hide_keyboard=True, selective=selective)
+
+
+class ForceReply(ReplyMarkup):
+    def __init__(self, selective=False):
+        super().__init__(force_reply=True, selective=selective)
 
 
 class ApiError(Exception):
