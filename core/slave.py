a7d28653578e9f5b8627d52bfd68f31345995e82
Andrey 'virus' Yantsen <andrey@janzen.su>
2017-02-08 20:00:03 +0300

    ignore "message to edit not found"

--- core/slave.py
+++ core/slave.py
@@ -245,7 +245,8 @@ class Slave(Base):
             except Exception as e:
                 # Ignore `message is not modified` and `message not found` errors
                 if not isinstance(e, ApiError) or e.code != 400 or ('message is not modified' not in e.description\
-                                                                    and 'message not found' not in e.description):
+                                                                    and 'message not found' not in e.description\
+                                                                    and 'message to edit not found' not in e.description):
                     raise
 
         yield self.db.execute('UPDATE incoming_messages SET is_voting_fail = TRUE WHERE bot_id = %s AND '
