252e970f7622f5a708bb5e9ed49b43486d652a4c
Andrey 'virus' Yantsen <andrey@janzen.su>
2017-04-13 09:33:42 +0300

    [refs #24] fix code&syntax; add translations

--- core/handlers/slave/reject.py
+++ core/handlers/slave/reject.py
@@ -27,21 +27,26 @@ def plaintext_reject_handler(bot, message, chat_id, message_id):
     msg = message['text'].strip()
     if len(msg) < 10:
         report_botan(message, 'slave_reply_short_msg')
-        yield bot.send_message(pgettext('Reject message is too short', 'Reject message is too short (10 symbols required), try '
-                                                                      'again or send /cancel'),
-                               reply_to_message=message, reply_markup=ForceReply(True))
+        yield bot.send_message(
+            pgettext('Reject message is too short', 'Reject message is too short (10 symbols required), try '
+                                                    'again or send /cancel'),
+            reply_to_message=message, reply_markup=ForceReply(True))
     else:
-	    yield bot.db.execute('UPDATE incoming_messages SET is_voting_fail = TRUE WHERE id = %s AND '
-							 'original_chat_id = %s',
-							 (message_id, original_chat_id))
-        try:
-			yield bot.send_message(pgettext('Message to user in case of rejection',
-                                            "Your post has been rejected. "
-                                            "Reason:\n> {reject_reason}").format(reject_reason=msg),
-                                   chat_id=user_id, reply_to_message_id=message_id)
-            yield bot.send_message(pgettext('Rejection delivery confirmation', 'Message sent and post rejected'), reply_to_message=message)
-        except Exception as e:
-            yield bot.send_message(pgettext('Rejection failed', 'Message sending failed: {reason}').format(reason=str(e)),
-                                   reply_to_message=message)
+        yield bot.decline_message({
+            'chat': {
+                'id': chat_id
+            },
+            'message_id': message_id
+        }, 0, False)
+    try:
+        yield bot.send_message(pgettext('Message to user in case of rejection',
+                                        "Your post has been rejected. "
+                                        "Reason:\n> {reject_reason}").format(reject_reason=msg),
+                               chat_id=chat_id, reply_to_message_id=message_id)
+        yield bot.send_message(pgettext('Rejection delivery confirmation', 'Message sent and post rejected'),
+                               reply_to_message=message)
+    except Exception as e:
+        yield bot.send_message(pgettext('Rejection failed', 'Message sending failed: {reason}').format(reason=str(e)),
+                               reply_to_message=message)
 
-        return True
+    return True
